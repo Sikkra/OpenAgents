aa1cb1cd6822018b391b2cadac4bd5d1a26bf255
Sikkra <159844544+Sikkra@users.noreply.github.com>
2026-05-19 23:57:41 -0500

    fix: harden yield aggregator accounting

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,8 +2,10 @@ require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: {
-    version: "0.8.20",
+    version: "0.8.24",
     settings: {
+      evmVersion: "cancun",
+      viaIR: true,
       optimizer: {
         enabled: true,
         runs: 200,
